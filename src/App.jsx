12c17ef14a02f7410c078452ec9d13256cb8e9e2
pixelsandstitchespixelsandstitches <srosentowski@gmail.com>
2026-06-14 00:11:17 -0700

    add landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Landing from './Landing'
 import WeeklyMealPlan from './WeeklyMealPlan'
 import Beachcrest from './beachcrest-google-sites'
 import BeachcrestRedesign from './beachcrest-redesign'
@@ -7,7 +8,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<WeeklyMealPlan />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/meal-plan" element={<WeeklyMealPlan />} />
         <Route path="/beachcrest" element={<Beachcrest />} />
         <Route path="/beachcrest-redesign" element={<BeachcrestRedesign />} />
       </Routes>
